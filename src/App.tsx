38be209d1604620c33915461c6a86e8638aeddee
nakjun <njsung1217@gmail.com>
2024-01-31 17:15:26 +0900

    feat: draft commit
    webgpu & react & typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { Initialize } from './test';
 
 function App() {
+  useEffect(() => {
+    Initialize();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <canvas id="gfx-main" width="800" height="600"></canvas>
     </div>
   );
 }
---
+++ src/particle.ts
@@ -0,0 +1,11 @@
+export class Particle {
+    position: [number, number];
+    color: [number, number, number];
+
+    constructor(position: [number, number], color: [number, number, number]) {
+        this.position = position;
+        this.color = color;
+    }
+
+    // Add methods for updating particle logic if needed
+}
