443ade6c4392b5b421f29806401c9de349759b2e
nakjun sung <njsung@1101.co.kr>
2024-02-12 18:19:24 +0900

    feat: add normal update shader(not complete)

--- src/[02].ClothSystem/ParticleShader.ts
+++ src/[02].ClothSystem/ParticleShader.ts
@@ -173,8 +173,30 @@ export class ParticleShader {
         positions[index*3 + 0] = pos.x;
         positions[index*3 + 1] = pos.y;
         positions[index*3 + 2] = pos.z;
+    }    
+    `;
+
+    normalUpdateShader = `
+    
+    @group(0) @binding(0) var<storage, read_write> positions: array<f32>;    
+    @group(0) @binding(1) var<storage, read_write> normals: array<f32>;
+
+    fn getPosition(index:u32) -> vec3<f32>{
+        return vec3<f32>(positions[index*3],positions[index*3+1],positions[index*3+2]);
     }
     
+    fn getNormals(index:u32) -> vec3<f32>{
+        return vec3<f32>(normals[index*3],normals[index*3+1],normals[index*3+2]);
+    }
+
+    @compute 
+    @workgroup_size(256)
+    fn main(@builtin(global_invocation_id) global_id: vec3<u32>) {
+        let index: u32 = global_id.x;
+
+        var pos = getPosition(index);
+        var normal = getNormals(index);
+    }    
     `;
 
 
